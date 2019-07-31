5970c781a9aca3abfecaa1c0461431b64e9719f7
Shubhendu Kumar <34783719+shubhendukr@users.noreply.github.com>
2019-07-31 10:03:34 +0800

    Forwarding the container port to a host

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,11 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=shubhendukumar/api
+dockerpath="shubhendukumar/api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run --image=shubhendukumar/api --port=80
+kubectl run apidemo\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=apidemo
 
 # Step 3:
 # List kubernetes pods
@@ -17,4 +20,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward apidemo 8000:80
